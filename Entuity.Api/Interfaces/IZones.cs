e483ca7960eff7280116fa03bf607f1fda9d08d4
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-02-19 10:41:21 +0000

    ENT-1, Added zones details call

--- Entuity.Api/Interfaces/IZones.cs
+++ Entuity.Api/Interfaces/IZones.cs
@@ -2,13 +2,31 @@
 using Refit;
 
 namespace Entuity.Api.Interfaces;
+
+/// <summary>
+/// Zones
+/// see https://support.entuity.com/hc/en-us/articles/13830161498909-Zones-RESTful-API
+/// see https://support.entuity.com/hc/en-us/articles/13830223150621-Zone-Details-RESTful-API
+/// </summary>
 public interface IZones
 {
 	/// <summary>
 	/// Get all Zones
 	/// </summary>
+	/// <exception cref="Exceptions.ApiException">Thrown when fails to make API call</exception>
 	/// <param name="cancellationToken"></param>
 	/// <returns></returns>
 	[Get("/api/zones")]
 	Task<ZonesResponse> GetAllAsync(CancellationToken cancellationToken);
+
+	/// <summary>
+	/// Get Zone Details
+	/// </summary>
+	/// <param name="id">Zone ID</param>
+	/// <param name="cancellationToken"></param>
+	/// <exception cref="Exceptions.ApiException">Thrown when fails to make API call</exception>
+	/// <returns></returns>
+	[Get("/api/zones/{id}")]
+	Task<ZoneDetails> GetAsync(int id,
+		CancellationToken cancellationToken);
 }
---
+++ Entuity.Api/Models/ZoneDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace Entuity.Api.Models;
+public class ZoneDetails
+{
+	public int Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+	public int Flags { get; set; }
+	public string Description { get; set; } = string.Empty;
+	public string V4Interface { get; set; } = string.Empty;
+	public string V6Interface { get; set; } = string.Empty;
+	public string DomainSuffix { get; set; } = string.Empty;
+	public string Proxy { get; set; } = string.Empty;
+	public string DevicePrefix { get; set; } = string.Empty;
+	public string HostFile { get; set; } = string.Empty;
+	public IEnumerable<string> DnsServers { get; set; } = [];
+}
