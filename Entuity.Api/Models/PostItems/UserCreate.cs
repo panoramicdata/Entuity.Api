3729882bef154746c4b04924b47b20723e2f3030
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-19 11:19:47 +0000

    ENT-1, Added documentation to User Create model

--- Entuity.Api/Models/PostItems/UserCreate.cs
+++ Entuity.Api/Models/PostItems/UserCreate.cs
@@ -1,5 +1,9 @@
 ﻿namespace Entuity.Api.Models.PostItems;
 
+/// <summary>
+/// Represents the properties required to create a user.
+/// <para>See <a href='https://support.entuity.com/hc/en-us/articles/13829697694877-Users-RESTful-API#post'/></para>
+/// </summary>
 public class UserCreate
 {
 	/// <summary>
