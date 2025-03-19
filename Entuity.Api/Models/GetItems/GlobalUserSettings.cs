7345d5e4f228d9eef6096ac86116c8f5a84c2ed1
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-19 11:32:53 +0000

    ENT-1, Updated global user settings documentation

--- Entuity.Api/Models/GetItems/GlobalUserSettings.cs
+++ Entuity.Api/Models/GetItems/GlobalUserSettings.cs
@@ -1,14 +1,33 @@
 ﻿namespace Entuity.Api.Models.GetItems;
 
+/// <summary>
+/// Holds details about Global User Settings.
+/// <para>See <a href='https://support.entuity.com/hc/en-us/articles/13829745428637-Users-Global-User-Settings-RESTful-API#get'/></para>
+/// </summary>
 public class GlobalUserSettings
 {
+	/// <summary>
+	/// Days to password change. Value -1 to disable
+	/// </summary>
 	public int PwChangeDays { get; set; }
 
+	/// <summary>
+	/// Number of days to display a warning before a password change is required. Value 0 to disable
+	/// </summary>
 	public int PwdChangeNoticePeriod { get; set; }
 
+	/// <summary>
+	/// Number of minutes after which session becomes inactive. Value -1 to disable
+	/// </summary>
 	public int TimeoutMinutes { get; set; }
 
+	/// <summary>
+	/// Number of failed login attempts before locking the account. Value -1 to disable
+	/// </summary>
 	public int LockAttempts { get; set; }
 
+	/// <summary>
+	/// Duration (minutes) the account is locked for after reaching maximum failed attempts
+	/// </summary>
 	public int LockDurationFailedAttempts { get; set; }
 }
