9bcf7210179df016181379c68a7785c4af14db60
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-19 11:49:45 +0000

    ENT-1, Added documentation to UserGroup model

--- Entuity.Api/Models/GetItems/UserGroup.cs
+++ Entuity.Api/Models/GetItems/UserGroup.cs
@@ -3,11 +3,21 @@ using System.Text.Json.Serialization;
 
 namespace Entuity.Api.Models.GetItems;
 
+/// <summary>
+/// Holds information about a User Group.
+/// <para>See <a href='https://support.entuity.com/hc/en-us/articles/13829674289693-User-Groups-RESTful-API#get'/></para>
+/// </summary>
 public class UserGroup : Entity<string>, IGetItem
 {
+	/// <summary>
+	/// Entuity server Id on which resource resides
+	/// </summary>
 	[JsonPropertyName("serverId")]
 	public required string ServerId { get; set; }
 
+	/// <summary>
+	/// User group name
+	/// </summary>
 	[JsonPropertyName("name")]
 	public required string Name { get; set; }
 }
