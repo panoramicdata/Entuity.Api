de0f3ddf7d4535473e4978287e454b76b9a05d56
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-20 15:40:07 +0000

    ENT-1, Updated documentation for Server Group Summary Info Models

--- Entuity.Api/Models/ConfigurationData/ServerGroupsSummaryInfo/Get/ServerGroupSummaryInformation.cs
+++ Entuity.Api/Models/ConfigurationData/ServerGroupsSummaryInfo/Get/ServerGroupSummaryInformation.cs
@@ -2,35 +2,84 @@
 
 namespace Entuity.Api.Models.ConfigurationData.ServerGroupsSummaryInfo.Get;
 
+/// <summary>
+/// Provides summary information for a Config Set
+/// <para>See <a href='https://support.entuity.com/hc/en-us/articles/13840342484253-Config-Sets-Summary-Info-RESTful-API#get'/></para>
+/// </summary>
 public class ServerGroupSummaryInformation : IGetItem
 {
+	/// <summary>
+	/// If sync of users is enabled in this config set, either 'true' or 'false'
+	/// </summary>
 	public bool SyncUsers { get; set; }
 
+	/// <summary>
+	/// If sync of user groups is enabled in this config set, either 'true' or 'false'
+	/// </summary>
 	public bool SyncUserGroups { get; set; }
 
+	/// <summary>
+	/// If sync of Views is enabled in this config set, either 'true' or 'false'
+	/// </summary>
 	public bool SyncViews { get; set; }
 
+	/// <summary>
+	/// ID of the config set
+	/// </summary>
 	public Guid ServerGroupId { get; set; }
 
+	/// <summary>
+	/// Name of the config set
+	/// </summary>
 	public string ServerGroupName { get; set; } = string.Empty;
 
+	/// <summary>
+	/// Description of the config set, if any
+	/// </summary>
 	public string Description { get; set; } = string.Empty;
 
+	/// <summary>
+	/// Timestamp of when the config set was created
+	/// </summary>
 	public int CreationTimeSec { get; set; }
 
+	/// <summary>
+	/// User who created the config set
+	/// </summary>
 	public string CreatedBy { get; set; } = string.Empty;
 
+	/// <summary>
+	/// If synchronization of the config set is paused
+	/// </summary>
 	public bool SyncPaused { get; set; }
 
+	/// <summary>
+	/// D of the current configuration change, referred to in the table under the Sync History tab on the Multi-Server Configuration page. Each time a config set is updated, the set gains a new configuration change ID. This ID is an auto-incrementing number
+	/// </summary>
 	public int ConfigurationChangeId { get; set; }
 
+	/// <summary>
+	/// Timestamp of when the config set was last modified
+	/// </summary>
 	public int LastModifiedSec { get; set; }
 
+	/// <summary>
+	/// User who last modified the config set
+	/// </summary>
 	public string LastModifiedBy { get; set; } = string.Empty;
 
+	/// <summary>
+	/// Number of servers in the config set
+	/// </summary>
 	public int MemberCount { get; set; }
 
+	/// <summary>
+	/// 
+	/// </summary>
 	public required SyncStatus SyncStatus { get; set; }
 
+	/// <summary>
+	/// Description of the config set's current sync status
+	/// </summary>
 	public string SyncStatusCategory { get; set; } = string.Empty;
 }
--- Entuity.Api/Models/ConfigurationData/ServerGroupsSummaryInfo/SyncStatus.cs
+++ Entuity.Api/Models/ConfigurationData/ServerGroupsSummaryInfo/SyncStatus.cs
@@ -1,16 +1,38 @@
 ﻿namespace Entuity.Api.Models.ConfigurationData.ServerGroupsSummaryInfo;
 
+/// <summary>
+/// Contains information about the synchronization status of a configuration set
+/// <para>See <a href='https://support.entuity.com/hc/en-us/articles/13840342484253-Config-Sets-Summary-Info-RESTful-API#get'/></para>
+/// </summary>
 public class SyncStatus
 {
+	/// <summary>
+	/// ID of the current configuration change, referred to in the table under the Sync History tab on the Multi-Server Configuration page. Each time a config set is updated, the set gains a new configuration change ID. This ID is an auto-incrementing number
+	/// </summary>
 	public int ConfigurationChangeId { get; set; }
 
+	/// <summary>
+	/// IDs of the servers in the config set
+	/// </summary>
 	public List<Guid>? MemberServerIds { get; set; }
 
+	/// <summary>
+	/// IDs of the servers currently synced
+	/// </summary>
 	public List<Guid>? ServerIdsAlreadySynced { get; set; }
 
+	/// <summary>
+	/// IDs of the servers currently queued to sync
+	/// </summary>
 	public List<Guid>? ServerIdsQueuedToSync { get; set; }
 
+	/// <summary>
+	/// IDs of the servers that have failed to sync, and the explanatory error message for each server that has failed to sync
+	/// </summary>
 	public List<Guid>? FailedServerIdsToErrorMsg { get; set; }
 
+	/// <summary>
+	/// If synchronization of the config set is paused
+	/// </summary>
 	public bool SyncPaused { get; set; }
 }
