177fb50c5497c65c231dd1f3f131eb9a1cb2fe25
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-20 16:11:49 +0000

    ENT-1, Updated documentation for Zone  create and update models

--- Entuity.Api/Models/ZonesData/Post/ZoneCreate.cs
+++ Entuity.Api/Models/ZonesData/Post/ZoneCreate.cs
@@ -2,28 +2,56 @@
 
 namespace Entuity.Api.Models.ZonesData.Post;
 
+/// <summary>
+/// Holds required and optional information to create a Zone
+/// <para>See <a href='https://support.entuity.com/hc/en-us/articles/13830161498909-Zones-RESTful-API#post'/></para>
+/// </summary>
 public class ZoneCreate
 {
+	/// <summary>
+	/// Name of the Zone
+	/// </summary>
 	public required string Name { get; set; }
 
+	/// <summary>
+	/// IPv4 interface for the zone
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public string? V4Interface { get; set; }
 
+	/// <summary>
+	/// IPv6 interface for the zone
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public string? V6Interface { get; set; }
 
+	/// <summary>
+	/// Domain suffix for the zone
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public string? DomainSuffix { get; set; }
 
+	/// <summary>
+	/// Currently unused <a href='https://support.entuity.com/hc/en-us/articles/13830161498909-Zones-RESTful-API#post'>as seen here</a>
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public string? Proxy { get; set; }
 
+	/// <summary>
+	/// Specified device prefix that is included with each device name found udner this zone, maximum of 5 characters
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public string? DevicePrefix { get; set; }
 
+	/// <summary>
+	/// Host File
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public string? HostFile { get; set; }
 
+	/// <summary>
+	/// List of DNS server IP Addresses
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public IEnumerable<string>? DnsServers { get; set; }
 
--- Entuity.Api/Models/ZonesData/Update/ZoneUpdate.cs
+++ Entuity.Api/Models/ZonesData/Update/ZoneUpdate.cs
@@ -2,35 +2,69 @@
 
 namespace Entuity.Api.Models.ZonesData.Update;
 
+/// <summary>
+/// Holds information to update a Zone
+/// <para>See <a href='https://support.entuity.com/hc/en-us/articles/13830223150621-Zone-Details-RESTful-API#put'/></para>
+/// </summary>
 public class ZoneUpdate
 {
+	/// <summary>
+	/// Zone Name
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public string? Name { get; set; }
 
+	/// <summary>
+	/// Zone Flags
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public int? Flags { get; set; }
 
+	/// <summary>
+	/// Description of the Zone
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public string? Description { get; set; }
 
+	/// <summary>
+	/// IPv4 interface for the Zone
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public string? V4Interface { get; set; }
 
+	/// <summary>
+	/// IPv6 interface for the Zone
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public string? V6Interface { get; set; }
 
+	/// <summary>
+	/// Domain suffix for the Zone
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public string? DomainSuffix { get; set; }
 
+	/// <summary>
+	/// Current unused <a href='https://support.entuity.com/hc/en-us/articles/13830223150621-Zone-Details-RESTful-API#put'>as seen here</a>
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public string? Proxy { get; set; }
 
+	/// <summary>
+	/// Specified device prefix that is included with each device name found under this zone, maximum of 5 characters
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public string? DevicePrefix { get; set; }
 
+	/// <summary>
+	/// Host File
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public string? HostFile { get; set; }
 
+	/// <summary>
+	/// List of DNS server IP Addresses
+	/// </summary>
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public IEnumerable<string>? DnsServers { get; set; }
 }
