3a8c26bf3f90988572500da0253e657f13d9e470
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-20 16:00:54 +0000

    ENT-1, Updated documentation for Auto Discovery

--- Entuity.Api/Collections/AutoDiscoveryResponse.cs
+++ Entuity.Api/Collections/AutoDiscoveryResponse.cs
@@ -2,7 +2,14 @@
 
 namespace Entuity.Api.Collections;
 
+/// <summary>
+/// Response model for AutoDiscovery data
+/// <para>See <a href='https://support.entuity.com/hc/en-us/articles/13829479167517-Auto-Discovery-Status-RESTful-API#get'/></para>
+/// </summary>
 public class AutoDiscoveryResponse
 {
+	/// <summary>
+	/// AutoDiscovery settings
+	/// </summary>
 	public IEnumerable<AutoDiscovery> Settings { get; set; } = [];
 }
--- Entuity.Api/Interfaces/Controllers/IAutoDiscovery.cs
+++ Entuity.Api/Interfaces/Controllers/IAutoDiscovery.cs
@@ -5,6 +5,11 @@ namespace Entuity.Api.Interfaces.Controllers;
 
 public interface IAutoDiscovery
 {
+	/// <summary>
+	/// Get all AutoDiscovery data
+	/// </summary>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	[Get("/api/autodiscovery")]
 	Task<AutoDiscoveryResponse> GetAllAsync(CancellationToken cancellationToken);
 
