1a1f9642199d72b3f20b5fc23cb6819f462cff1a
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-21 10:09:32 +0000

    ENT-1, Added documentation to GetAll method of user defined rest pollers

--- Entuity.Api/Interfaces/Controllers/IUserDefinedRestPollers.cs
+++ Entuity.Api/Interfaces/Controllers/IUserDefinedRestPollers.cs
@@ -6,6 +6,11 @@ namespace Entuity.Api.Interfaces.Controllers;
 
 public interface IUserDefinedRestPollers
 {
+	/// <summary>
+	/// Get all user defined REST pollers
+	/// </summary>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	[Get("/api/ud/pollers")]
 	public Task<Response<RestPoller>> GetAllAsync(CancellationToken cancellationToken);
 }
