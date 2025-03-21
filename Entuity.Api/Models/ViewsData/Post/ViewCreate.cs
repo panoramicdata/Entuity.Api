93be3bc90c1af1cef5345af97432dad39d3e2f07
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-21 09:24:04 +0000

    ENT-1, Updated view create model to contain all possible optional properties

--- Entuity.Api/Models/ViewsData/Post/ViewCreate.cs
+++ Entuity.Api/Models/ViewsData/Post/ViewCreate.cs
@@ -1,8 +1,57 @@
-﻿namespace Entuity.Api.Models.ViewsData.Post;
+﻿using System.Text.Json.Serialization;
 
+namespace Entuity.Api.Models.ViewsData.Post;
+
+/// <summary>
+/// Model for creating a new View
+/// <para>See <a href='https://support.entuity.com/hc/en-us/articles/13829756254109-View-List-RESTful-API#post'/></para>
+/// </summary>
 public class ViewCreate
 {
+	/// <summary>
+	/// Name of the View being created
+	/// </summary>
 	public required string Name { get; set; }
 
-	// The rest of the properties are optional
+	/// <summary>
+	/// Array of access objects, specifying view access permissions
+	/// </summary>
+	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+	public IEnumerable<AccessGroup>? AccessGroups { get; set; }
+
+	/// <summary>
+	/// A way to aggregate base views: One of NONE (default), UNION or INTERSECTION
+	/// </summary>
+	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+	public string? BaseViewAggregation { get; set; }
+
+	/// <summary>
+	/// Array of base views
+	/// </summary>
+	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+	public IEnumerable<object>? BaseViewPaths { get; set; }
+
+	/// <summary>
+	/// Name of the domain filter to use
+	/// </summary>
+	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+	public string? DomainFilterName { get; set; }
+
+	/// <summary>
+	/// Username of the user who will be an owner of the View. This defaults to the user making a call. Only administrators may specify a user other than themselves
+	/// </summary>
+	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+	public string? Owner { get; set; }
+
+	/// <summary>
+	/// Forward-slash separated path of the parent view. Leave out to create a top-level view
+	/// </summary>
+	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+	public string? ParentViewPath { get; set; }
+
+	/// <summary>
+	/// Location of the View, which is used by the Map dashlet in Geographical Mode
+	/// </summary>
+	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+	public string? Location { get; set; }
 }
