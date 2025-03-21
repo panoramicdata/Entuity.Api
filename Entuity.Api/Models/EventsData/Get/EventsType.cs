efba779fe968bb3a041d4506d3c0a68ebe624feb
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-21 10:46:39 +0000

    ENT-1, Added missing documentation for events type

--- Entuity.Api/Models/EventsData/Get/EventsType.cs
+++ Entuity.Api/Models/EventsData/Get/EventsType.cs
@@ -4,18 +4,19 @@ using System.Text.Json.Serialization;
 namespace Entuity.Api.Models.EventsData.Get;
 
 /// <summary>
-/// 
+/// Represents an event type.
+/// <para>See <a href='https://support.entuity.com/hc/en-us/articles/13819768912669-Event-Types-RESTful-API#get'/></para>
 /// </summary>
 public class EventsType : Entity<int>, IGetItem
 {
 	/// <summary>
-	/// 
+	/// Name of the event type
 	/// </summary>
 	[JsonPropertyName("name")]
 	public required string Name { get; set; }
 
 	/// <summary>
-	/// 
+	/// Severity of the event type
 	/// </summary>
 	[JsonPropertyName("severity")]
 	public required int Severity { get; set; }
