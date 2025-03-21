6e4fb8759fb142a1ca3b4371866642686edd8d1e
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-21 09:43:00 +0000

    ENT-1, Updated documentation for user groups models and controller methods

--- Entuity.Api/Interfaces/Controllers/IUserGroups.cs
+++ Entuity.Api/Interfaces/Controllers/IUserGroups.cs
@@ -6,15 +6,39 @@ using Refit;
 namespace Entuity.Api.Interfaces.Controllers;
 public interface IUserGroups
 {
+	/// <summary>
+	/// Get all User Groups
+	/// </summary>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	[Get("/api/userGroups")]
 	Task<Response<UserGroup>> GetAllAsync(CancellationToken cancellationToken);
 
+	/// <summary>
+	/// Get a User Group by Id
+	/// </summary>
+	/// <param name="userGroup"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	[Post("/api/userGroups")]
 	Task<Response<UserGroup>> CreateAsync([Body] UserGroupCreate userGroup, CancellationToken cancellationToken);
 
+	/// <summary>
+	/// Delete a User Group by Id
+	/// </summary>
+	/// <param name="id"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	[Delete("/api/userGroups/{id}")]
 	Task<Response<UserGroup>> DeleteAsync(int id, CancellationToken cancellationToken);
 
+
+	/// <summary>
+	/// Get all tools for a User Group
+	/// </summary>
+	/// <param name="id"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	[Get("/api/userGroups/{id}/tools")]
 	Task<Response<UserGroupTool>> GetToolsAsync(int id, CancellationToken cancellationToken);
 }
--- Entuity.Api/Models/UserGroupsData/Get/UserGroupTool.cs
+++ Entuity.Api/Models/UserGroupsData/Get/UserGroupTool.cs
@@ -2,9 +2,19 @@
 
 namespace Entuity.Api.Models.UserGroupsData.Get;
 
+/// <summary>
+/// Represents a User Group Tool
+/// <para>See <a href='https://support.entuity.com/hc/en-us/articles/13829721504285-User-Group-Permissions-RESTful-API#get'/></para>
+/// </summary>
 public class UserGroupTool : Entity<int>, IGetItem
 {
+	/// <summary>
+	/// Name of the Tool
+	/// </summary>
 	public string ToolName { get; set; } = string.Empty;
 
+	/// <summary>
+	/// Name of the subgroup that given tool belongs to
+	/// </summary>
 	public string GroupName { get; set; } = string.Empty;
 }
