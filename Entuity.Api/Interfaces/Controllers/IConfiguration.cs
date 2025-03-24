058cbda28210cd98feb05fbf4cd4448bfdb7301e
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-24 16:15:04 +0000

    ENT-1, Updated documentation for configuration methods

--- Entuity.Api/Interfaces/Controllers/IConfiguration.cs
+++ Entuity.Api/Interfaces/Controllers/IConfiguration.cs
@@ -13,33 +13,94 @@ namespace Entuity.Api.Interfaces.Controllers;
 
 public interface IConfiguration
 {
+	/// <summary>
+	/// Get all Server Groups
+	/// </summary>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	[Get("/api/cfg/serverGroups")]
 	public Task<Response<ServerGroup>> GetServerGroupsAsync(CancellationToken cancellationToken);
 
+	/// <summary>
+	/// Create new Server Group
+	/// </summary>
+	/// <param name="serverGroup"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	[Post("/api/cfg/serverGroups")]
 	public Task<ServerGroup> CreateServerGroupAsync([Body] ServerGroupCreate serverGroup, CancellationToken cancellationToken);
 
+	/// <summary>
+	/// Get a Server Group by ID
+	/// </summary>
+	/// <param name="serverGroupId"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	[Get("/api/cfg/serverGroups/{serverGroupId}")]
 	public Task<ServerGroup> GetServerGroupAsync(Guid serverGroupId, CancellationToken cancellationToken);
 
+	/// <summary>
+	/// Delete a Server Group by ID
+	/// </summary>
+	/// <param name="serverGroupId"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	[Delete("/api/cfg/serverGroups/{serverGroupId}")]
 	public Task<DeleteResponse> DeleteServerGroupAsync(Guid serverGroupId, CancellationToken cancellationToken);
 
+	/// <summary>
+	/// Update a Server Group by ID
+	/// </summary>
+	/// <param name="serverGroupId"></param>
+	/// <param name="serverGroup"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	[Put("/api/cfg/serverGroups/{serverGroupId}")]
 	public Task<ServerGroup> UpdateServerGroupAsync(Guid serverGroupId, [Body] ServerGroupUpdate serverGroup, CancellationToken cancellationToken);
 
+	/// <summary>
+	/// Get the server group configuration for a server group by ID
+	/// </summary>
+	/// <param name="serverGroupId"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	[Get("/api/cfg/serverGroupConfig/{serverGroupId}")]
 	public Task<ServerGroupConfiguration> GetServerGroupConfigurationAsync(Guid serverGroupId, CancellationToken cancellationToken);
 
+	/// <summary>
+	/// Get all Server Group Summary Information
+	/// </summary>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	[Get("/api/cfg/serverGroupsSummaryInfo")]
 	public Task<Response<ServerGroupSummaryInformation>> GetServerGroupsSummaryInfoAsync(CancellationToken cancellationToken);
 
+	/// <summary>
+	/// Get the server group memberships for a server group by ID
+	/// </summary>
+	/// <param name="serverGroupId"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	[Get("/api/cfg/serverGroupMembership/{serverGroupId}")]
 	public Task<Response<ServerGroupMembership>> GetServerGroupMembershipsAsync(Guid serverGroupId, CancellationToken cancellationToken);
 
+	/// <summary>
+	/// Add a new server group membership for a server group by ID
+	/// </summary>
+	/// <param name="serverGroupId"></param>
+	/// <param name="serverGroupMembership"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	[Post("/api/cfg/serverGroupMembership/{serverGroupId}")]
 	public Task<Response<ServerGroupMembership>> AddServerGroupMembershipAsync(Guid serverGroupId, [Body] ServerGroupMembershipAdd serverGroupMembership, CancellationToken cancellationToken);
 
+	/// <summary>
+	/// Remove a server group membership for a server group by ID
+	/// </summary>
+	/// <param name="serverGroupId"></param>
+	/// <param name="serverGroupMembership"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	[Post("/api/cfg/serverGroupMembership/{serverGroupId}")]
 	public Task<Response<ServerGroupMembership>> RemoveServerGroupMembershipAsync(Guid serverGroupId, [Body] ServerGroupMembershipRemove serverGroupMembership, CancellationToken cancellationToken);
 }
