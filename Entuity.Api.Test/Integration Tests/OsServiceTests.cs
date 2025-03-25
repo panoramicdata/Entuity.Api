0ef7b69fd9ae89d9d97ea1082984a50d13845b11
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-03-25 10:07:52 +0000

    ENT-1, Added get single os service rule method and test

--- Entuity.Api.Test/Integration Tests/OsServiceTests.cs
+++ Entuity.Api.Test/Integration Tests/OsServiceTests.cs
@@ -12,4 +12,23 @@ public class OsServiceTests(EntuityClient client) : TestFixture
 
 		response.Should().NotBeNull();
 	}
+
+	[Fact]
+	public async Task OsServicesController_GetAsync_Succeeds()
+	{
+		var response = await client
+			.OsServices
+			.GetAllAsync(default);
+
+		response.Should().NotBeNull();
+
+		foreach (var osService in response.ServiceRules)
+		{
+			var osServiceResponse = await client
+				.OsServices
+				.GetAsync(osService.Id, default);
+
+			osServiceResponse.Should().NotBeNull();
+		}
+	}
 }
--- Entuity.Api/Interfaces/Controllers/IOsServices.cs
+++ Entuity.Api/Interfaces/Controllers/IOsServices.cs
@@ -12,4 +12,13 @@ public interface IOsServices
 	/// <returns></returns>
 	[Get("/api/osService")]
 	Task<OsServiceResponse> GetAllAsync(CancellationToken cancellationToken);
+
+	/// <summary>
+	/// Get a specific Os Service rule by ID
+	/// </summary>
+	/// <param name="id"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[Get("/api/osService/{id}")]
+	Task<OsService> GetAsync(int id, CancellationToken cancellationToken);
 }
